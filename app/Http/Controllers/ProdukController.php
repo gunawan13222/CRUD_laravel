0e8c2701fd5c5cb1db034d5e6812b0f56d8d7faa
Gun ridho <gunz1322@gmail.com>
2022-10-23 14:37:36 +1030

    fist commit

---
+++ app/Http/Controllers/ProdukController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\produk;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProdukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('produk.produk');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('produk.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nim'=>'required|numeric|uniqe:produks,nim',
+            'nama'=>'required:produks,nama',
+            'nim'=>'required:produks,jurusan',
+        ]);
+       $data = [
+        'nim'=>$request->nim,
+        'nama'=>$request->nama,
+        'jurusan'=>$request->jurusan,
+       ];
+       produk::create($data);
+
+
+    return 'berhasil kawan';
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\produk  $produk
+     * @return \Illuminate\Http\Response
+     */
+    public function show(produk $produk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\produk  $produk
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(produk $produk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\produk  $produk
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, produk $produk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\produk  $produk
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(produk $produk)
+    {
+        //
+    }
+}
